7c12b9f695c9f303ab72a125593f37160c464217
Lee Morgan <lee@leemorgan.dev>
2024-10-30 11:37:24 -0400

    Create header for vendor page.

---
+++ src/routes/[url]/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+    import "../../global.css";
+    import logo from "$lib/logo.png";
+    import Loader from "../../components/Loader.svelte";
+    import Notifier from "../../components/Notifier.svelte";
+    import Header from "./Header.svelte";
+
+    let {data} = $props();
+    let page = $state("products");
+    let loader = $state(false);
+    let vendor = $state({});
+</script>
+
+{#if loader}
+    <Loader/>
+{/if}
+
+<Header name={vendor.name}/>
+
+{#if page === "products"}
+
+{/if}
---
+++ src/routes/[url]/Header.svelte
@@ -0,0 +1,62 @@
+<script>
+    import {createEventDispatcher} from "svelte";
+    import logo from "$lib/logo.png";
+
+    const dispatch = createEventDispatcher();
+
+    let {name} = $props();
+
+    const changePage = (page)=>{
+        dispatch("changePage", {page: page});
+    }
+</script>
+
+<header>
+    <img src={logo} alt="Inlet.Shop logo">
+    <h3>Inlet.Shop > {name}</h3>
+
+    <div class="pages">
+        <button onclick={()=>{changePage("about")}}>About</button>
+        <button onclick={()=>{changePage("shop")}}>Shop</button>
+    </div>
+</header>
+
+<style>
+    header{
+        display: flex;
+        align-items: center;
+        height: 50px;
+        width: 100%;
+        background: black;
+        border-bottom: 1px solid rgba(255, 0, 0, 0.35);
+        padding: 0 35px;
+        color: var(--text);
+    }
+
+    header img{
+        height: 90%;
+    }
+
+    header h3{
+        font-size: 24px;
+        margin-left: 15px;
+    }
+
+    .pages{
+        margin-left: auto;
+    }
+
+    .pages button{
+        background: none;
+        border: none;
+        color: var(--text);
+        font-size: 22px;
+        text-decoration: underline;
+        margin: 0 35px;
+        cursor: pointer;
+    }
+
+    .pages button:hover{
+        color: rgb(175, 175, 175);
+    }
+</style>
