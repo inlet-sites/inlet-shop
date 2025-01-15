eed3328dd53d7e227140880e5d20caae440bb89a
Lee Morgan <lee@leemorgan.dev>
2025-01-15 18:01:35 -0500

    Create base for order display page.

--- src/routes/order/[orderId]/[orderToken]/+page.js
+++ src/routes/order/[orderId]/[orderToken]/+page.js
@@ -1,5 +1,5 @@
 export const load = async ({fetch, params})=>{
-    const orderUrl = `${import.meta.env.VITE_API_URL}/order/${params.orderId}?token=${params.orderToken}`;
+    const orderUrl = `${import.meta.env.VITE_API_URL}/order/${params.orderId}/token/${params.orderToken}`;
     const orderData = await fetch(orderUrl, {
         method: "get",
         headers: {
--- src/routes/order/[orderId]/[orderToken]/+page.svelte
+++ src/routes/order/[orderId]/[orderToken]/+page.svelte
@@ -1,6 +1,53 @@
 <script>
     import Header from "../../../../components/Header.svelte";
+    import CartItem from "../../../../components/CartItem.svelte";
+
     let {data} = $props();
+    console.log(data.order.items);
+
+    const formatDate = (date)=>{
+        const formDate = new Date(date);
+
+        return formDate.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        });
+    }
+
+    const formatItem = (item)=>{
+        let variation;
+        for(let i = 0; i < item.product.variations.length; i++){
+            if(item.product.variations[i].id === item.variation){
+                variation = item.product.variations[i];
+                break;
+            }
+        }
+
+        return {
+            product: item.product,
+            variation: variation,
+            quantity: item.quantity
+        };
+    }
+
+    const formatPrice = (price)=>{
+        return `$${(price / 100).toFixed(2)}`;
+    }
+
+    const calculatePrice = ()=>{
+        let price = 0;
+        for(let i = 0; i < data.order.items.length; i++){
+            const item = data.order.items[i];
+            for(let j = 0; j < item.product.variations.length; j++){
+                const variation = item.product.variations[j];
+                if(variation.id === item.variation){
+                    price += variation.price * item.quantity;
+                }
+            }
+        }
+        return price;
+    }
 </script>
 
 <Header/>
@@ -12,9 +59,47 @@
             <a href="/">Return to Shopping</a>
         </div>
     {:else}
-        <h1>{data.order.vendor}</h1>
-        <h2>{data.order.name}</h2>
-        <h3>Something</h3>
+        <table>
+            <tbody>
+                <tr>
+                    <td>Store:</td>
+                    <td>{data.order.vendor[0].store}</td>
+                </tr>
+                <tr>
+                    <td>Customer:</td>
+                    <td>{data.order.name}</td>
+                </tr>
+                <tr>
+                    <td>Shipping Address:</td>
+                    <td>{data.order.address}</td>
+                </tr>
+                <tr>
+                    <td>Order Date:</td>
+                    <td>{formatDate(data.order.date)}</td>
+                </tr>
+                <tr>
+                    <td>Price:</td>
+                    <td>{formatPrice(calculatePrice())}</td>
+                </tr>
+                <tr>
+                    <td>Shipping</td>
+                    <td>???</td>
+                </tr>
+                <tr>
+                    <td>Total:</td>
+                    <td>???</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h2>Items</h2>
+        {#each data.order.items as item}
+            <CartItem
+                item={formatItem(item)}
+                vendor={data.order.vendor[0].store}
+                edit={false}
+            />
+        {/each}
     {/if}
 </div>
 
