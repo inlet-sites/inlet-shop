9c935d79ffe1da55367171cfadd193d47444bfd8
Lee Morgan <lee@leemorgan.dev>
2025-03-14 15:35:46 -0400

    Create the about page.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,6 +4,28 @@
 
 <div class="container">
     <Header/>
+        
+    <h1>Inlet.Shop</h1>
+    <p>Inlet.Shop is an online shopping mall for Murrells Inlet and the surrounding area. We aim to assist businesses in the area by providing a cheap and easy platform to reach customers online and sell or display their items. We also hope to help customers to purchase locally. By creating a local online market, we hope that people will be able to better find and purchase locally made/produced goods and support the community.</p>
 
-    <h1></h1>
+    <p>Created in early 2025, Inlet.Shop was created by <a href="https://inletsites.dev">Inlet Sites</a>, a software development company based locally, right in Murrells Inlet.</p>
+
+    <p>We hope that as a customer, you can discover products and shops right here locally, whether you purchase through the Inlet.Shop platform, or visit the businesses in person.</p>
+
+    <p>We hope that as a business, you can sell your items here as cheap as possible and have new customers driven to your business</p>
 </div>
+
+<style>
+    .container{
+        color: var(--text);
+    }
+
+    h1{
+        margin: 35px;
+    }
+
+    p{
+        font-size: 18px;
+        margin: 15px 35px;
+    }
+</style>
