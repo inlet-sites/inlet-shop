b1092fcc20e30a7080f2c1ba4f4f4711a4d87d55
Lee Morgan <lee@leemorgan.dev>
2025-04-23 10:23:15 -0400

    Bug fix: use public data image.

--- src/routes/VendorCard.svelte
+++ src/routes/VendorCard.svelte
@@ -6,7 +6,7 @@
 <a class="VendorCard" href="/{vendor.url}/shop">
     <div class="imageContainer">
         {#if vendor.publicData.image}
-            <img src="{apiUrl}/document/{vendor.image}" alt={vendor.store}>
+            <img src="{apiUrl}/document/{vendor.publicData.image}" alt={vendor.store}>
         {:else}
             <img src="{apiUrl}/document/defaultVendorImage.png" alt={vendor.store}>
         {/if}
